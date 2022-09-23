71bd577be0f4a5db428316e3a7c5985cf36603bc
code1218 <skjil1218@gmail.com>
2022-09-23 13:08:04 +0900

    mybatis 세팅 및 insert

---
+++ src/main/java/com/boot/mybatis20220923junil/controller/api/NewsController.java
@@ -0,0 +1,33 @@
+package com.boot.mybatis20220923junil.controller.api;
+
+import com.boot.mybatis20220923junil.domain.News;
+import com.boot.mybatis20220923junil.dto.CMRespDto;
+import com.boot.mybatis20220923junil.dto.NewsWriteReqDto;
+import com.boot.mybatis20220923junil.repository.NewsRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api")
+@RequiredArgsConstructor
+@RestController
+public class NewsController {
+
+    private final NewsRepository newsRepository;
+
+    @PostMapping("/news")
+    public ResponseEntity<?> write(NewsWriteReqDto newsWriteReqDto) {
+
+        log.info("{}", newsWriteReqDto);
+
+        News news = newsWriteReqDto.toEntity();
+        news.setNews_writer("김준일");
+        newsRepository.save(news);
+
+        return ResponseEntity.ok(new CMRespDto<>(1, "새 글 작성 완료", null));
+    }
+}
